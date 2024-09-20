6aa77c218e547dc3e5f25abaacc6e565336c4af6
Mike Neal <mike@miken.dev>
2024-09-20 16:22:11 -0400

    add winbar

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -7,13 +7,22 @@ return {
 				theme = 'dracula'
 			},
 			sections = {
-				    lualine_a = {'mode'},
-				    lualine_b = {'branch', 'diff', 'diagnostics'},
-				    lualine_c = {{'filename', path = 1}},
-				    lualine_x = {'encoding', 'fileformat', 'filetype'},
-				    lualine_y = {'progress'},
-				    lualine_z = {'location'}
+			    lualine_a = {'mode'},
+				lualine_b = {'branch', 'diff', 'diagnostics'},
+				lualine_c = {{'filename', path = 1}},
+				lualine_x = {},
+				lualine_y = {'mason'},
+			    lualine_z = {'location'}
+			},
+			winbar = {
+				lualine_a = {'mode'},
+				lualine_b = {'tabs'},
+				lualine_c = {{'filename', path = 1}},
+				lualine_x = {'progress', 'location'},
+				lualine_y = {'encoding', 'fileformat', 'filetype'},
+				lualine_z = {'branch', 'diff', 'diagnostics'}
 			}
+
 		})
 	end
 }
