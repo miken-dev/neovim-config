84ab7eaf7820e7d52856ee277e4e08e1e2b1f70c
Mike Neal <mike@miken.dev>
2024-09-20 13:03:22 -0400

    add neoscroll and update options

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -7,4 +7,23 @@ vim.opt.signcolumn = "auto" -- sets vim.opt.signcolumn to auto
 vim.opt.wrap = false -- sets vim.opt.wrap
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
+vim.opt.autoindent = true
+
+-- search settings
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.cursorline = true
+vim.opt.signcolumn = "yes"
+
+vim.opt.backspace = "indent,eol,start"
+
+vim.opt.clipboard:append("unnamedplus")
+
+-- split windows
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
 colorcolumn = 80
+
