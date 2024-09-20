1451071093f576e935a26e7a80e7d11f925f856d
Mike Neal <mike@miken.dev>
2024-09-20 16:22:42 -0400

    update telescope config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,10 +3,18 @@ return {
 		'nvim-telescope/telescope.nvim', tag = '0.1.8',
 		dependencies = { 'nvim-lua/plenary.nvim' },
 		config = function()
+			local telescope = require("telescope")
 			local builtin = require("telescope.builtin")
+			telescope.setup({
+				defaults = {
+					path_display = {"smart"}
+				}
+			})
 			vim.keymap.set('n', '<C-o>', builtin.find_files, {}) 
-			vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "find files" })
-			vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "grep search" })
+			vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Find files in directory" })
+			vim.keymap.set('n', '<leader>fs', builtin.live_grep, { desc = "Find string in directory" })
+			vim.keymap.set('n', '<leader>fr', "<cmd>Telescope oldfiles<cr>", { desc = "Find recent files" })
+			vim.keymap.set('n', '<leader>fc', "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor" })
 		end
 	},
 	{
