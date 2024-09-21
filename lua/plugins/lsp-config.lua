2b187ca2cab3fd51cbf626af0777ceee3b1fd5cd
Mike Neal <mike@miken.dev>
2024-09-21 02:50:22 -0400

    update lsp configuration

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,26 +1,45 @@
 return {
 	{
 		"williamboman/mason.nvim",
+		lazy = false,
 		config = function()
-			require("mason").setup()	
-		end
+			require("mason").setup()
+		end,
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
-		config = function() 
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "ts_ls", "svelte", "markdown_oxide" }
-			})
-		end
+		lazy = false,
+		opts = {
+			auto_install = true,
+		},
 	},
 	{
 		"neovim/nvim-lspconfig",
+		lazy = false,
 		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
 			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({})
-			lspconfig.ts_ls.setup({})
-			lspconfig.svelte.setup({})
-			lspconfig.markdown_oxide.setup({})
-		end
-	}
+			lspconfig.ts_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.svelte.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.solargraph.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.html.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
