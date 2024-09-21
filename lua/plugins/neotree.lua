0d7686b5b621a58a2979b577494a21d5e36190ef
Mike Neal <mike@miken.dev>
2024-09-21 14:15:09 -0400

    fix formatting

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,28 +1,28 @@
 return {
-		"nvim-neo-tree/neo-tree.nvim",
-		branch = "v3.x",
-    		dependencies = {
-      			"nvim-lua/plenary.nvim",
-			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-			"MunifTanjim/nui.nvim",
-    		},
-		config = function()
-			require("neo-tree").setup({
-				source_selector = {
-				    winbar = true,
-				    statusline = false
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		require("neo-tree").setup({
+			source_selector = {
+				winbar = true,
+				statusline = true,
+			},
+			window = {
+				mappings = {
+					["p"] = { "toggle_preview", config = { use_float = true, use_image_nvim = true } },
 				},
-				  window = {
-				  	mappings = {
-						["p"] = { "toggle_preview", config = { use_float = true, use_image_nvim = true } },
-					}
-				}
-			})
-			vim.keymap.set('n', '<leader>e',':Neotree filesystem reveal left<CR>')
-			buffers = {
-				follow_current_file = {
-					enabled = true
-				}
+			},
+		})
+		vim.keymap.set("n", "<leader>e", ":Neotree filesystem reveal left<CR>")
+		buffers = {
+			follow_current_file = {
+				enabled = true,
+			},
 		}
-		end
-	}
+	end,
+}
