18c61272fc1362572e618837ce4f58cf8b594688
Mike Neal <mike@miken.dev>
2024-09-23 14:18:14 -0400

    add dependencies and swift language server

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -11,8 +11,19 @@ return {
 	},
 	{
 		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-buffer",  -- source for text in buffer
+			"hrsh7th/cmp-path",    -- source for file system paths
+			"L3MON4D3/LuaSnip",    -- snippet engine
+			"saadparwaiz1/cmp_luasnip", -- for autocompletion
+			"rafamadriz/friendly-snippets", -- useful snippets
+			"onsails/lspkind.nvim", -- vs-code like pictograms
+		},
+
 		config = function()
 			local cmp = require("cmp")
+			local lspkind = require("lspkind")
+
 			require("luasnip.loaders.from_vscode").lazy_load()
 
 			cmp.setup({
@@ -38,6 +49,12 @@ return {
 				}, {
 					{ name = "buffer" },
 				}),
+				formatting = {
+					format = lspkind.cmp_format({
+						maxwidth = 50,
+						ellipsis_char = "...",
+					}),
+				},
 			})
 		end,
 	},
