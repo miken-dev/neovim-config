6aa586388e5490f9e5e7adf5f764efd33c3be2f2
Mike Neal <mike@miken.dev>
2024-09-23 01:07:52 -0400

    update which-key config

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,16 +1,28 @@
 return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
- opts = {
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	config = function()
+		local wk = require("which-key")
+		wk.add({
+			{ "<leader>g", group = "git..." },
+			{ "<leader>w", group = "window/split..." },
+			{ "<leader>t", group = "tabs..." },
+			{ "<leader>c", group = "code..." },
+			{ "<leader>d", group = "debugger..." },
+			{ "<leader>f", group = "find..." },
+			{ "<leader>j", group = "todo's..." },
+			{ "<leader>r", group = "run tests" },
+			{ "<leader>s", group = "search highlights" },
+		})
+	end,
+	opts = {},
+	keys = {
+		{
+			"<leader>h",
+			function()
+				require("which-key").show({ global = false })
+			end,
+			desc = "Buffer Local Keymaps (which-key)",
+		},
 	},
-  keys = {
-    {
-      "<leader>h",
-      function()
-        require("which-key").show({ global = false })
-      end,
-      desc = "Buffer Local Keymaps (which-key)",
-    },
-  },
 }
-
