e8b1cc748a87bd0014044a96cf2ce728361f6f71
Michael Neal <mike@miken.dev>
2025-01-14 01:14:14 -0500

    update stacks config

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -8,14 +8,13 @@ return {
 		dashboard = { 
 			enabled = true,
 			sections = {
-				{ section = "header" },
-				{ section = "keys", gap = 1, padding = 1},
-				{ section = "startup", padding = 3 },
+				{ section = "header", pane = 2 },
+				{ section = "keys", gap = 1, padding = 1, pane = 2},
+				{ section = "startup", padding = 3, pane = 2 },
 				{
 					section = "terminal",
 					cmd = "ascii-image-converter ~/.config/logo-2.png -C -c",
 					random = 10,
-					pane = 2,
 					indent = 4,
 					height = 25
 				}
@@ -74,7 +73,7 @@ return {
 				Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
 				Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
 				Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
-				Snacks.toggle.diagnostics():map("<leader>ud")
+--				Snacks.toggle.diagnostics():map("<leader>ud")
 				Snacks.toggle.line_number():map("<leader>ul")
 				Snacks.toggle.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 })
 					:map("<leader>uc")
